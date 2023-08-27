fc149c1c350c749e452f7c86d8fb47542226f2a2
michael <mikedami001@gmail.com>
2023-08-27 03:10:43 -0700

    del

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,6 +0,0 @@
-# this manifest changes SSH config
-
-exec {'add_ssh_config':
-command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-path => 'usr/bin:/bin',
-}
