fa5efac464b75ac57e9c5716ba1454a6f6699c5d
michael <mikedami001@gmail.com>
2023-08-30 04:44:48 -0700

    ll

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# this manifest creates school in tmp
+
+# Using Puppet, create a file school in /tmp.
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
